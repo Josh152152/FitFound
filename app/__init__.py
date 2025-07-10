cc3b3c81ac23f9ce2e7a180db038e6dcb1bee29b
Josh152152 <sebastien.giard.fr@gmail.com>
2025-07-10 20:57:26 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-load_dotenv()  # loads variables from .env if running locally
+# Load .env variables if running locally (not needed on Render but doesn't hurt)
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY", "change_me!")
 
-# Enable CORS for Webflow site
+# --- Enable CORS ONLY for your Webflow frontend! ---
 from flask_cors import CORS
-CORS(app, origins=["https://fitfound.webflow.io"])
+CORS(app, origins=["https://fitfound.webflow.io"], supports_credentials=True)
+# If you ever need to allow local testing: origins=["https://fitfound.webflow.io", "http://localhost:3000"]
 
-from app import routes  # this should import your main routes.py
+from app import routes  # Import your main routes.py (must be after app is created)
