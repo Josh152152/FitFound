fe3cfb033ec3598c3ddd208138d1b2e7cdb678e8
Josh152152 <sebastien.giard.fr@gmail.com>
2025-07-13 15:45:02 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
 from app import create_app
 
+# Create the Flask app using the factory function
 app = create_app()
 
 if __name__ == "__main__":
+    # Get the port number from environment variables or default to 5000
     port = int(os.environ.get("PORT", 5000))
+    
+    # Run the Flask app on all IPs (0.0.0.0) to make it accessible externally, with the specified port
     app.run(host="0.0.0.0", port=port, debug=True)
